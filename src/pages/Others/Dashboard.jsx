38e4c6c94a36c7e2007005451fc1cefd4131648a
Dan George <dangeorge35@gmail.com>
2023-02-24 20:47:29 +0100

    Updated

--- src/pages/Others/Dashboard.jsx
+++ src/pages/Others/Dashboard.jsx
@@ -5,11 +5,19 @@ const Dashboard = () => {
   return (
     <div className="">
       <p>
-        <div className="text-sm">
-          <span className="text-lg mr-3">👋 </span>Hi Admin!
+        <div className="text-base">
+          <span className="text-2xl mr-1">👋 </span>Hi Admin!
         </div>
-        <div className="text-2xl pt-1">Welcome to your dashboard</div>
+        <div className="text-xl pt-1">Welcome to your dashboard</div>
       </p>
+      <div className="mt-8">
+        <div className="grid grid-cols-4 grid-rows-1 gap-4">
+          <div className="bg-white rounded p-3 h-16">1</div>
+          <div className="bg-white rounded p-3 h-16">2</div>
+          <div className="bg-white rounded p-3 h-16">3</div>
+          <div className="bg-white rounded p-3 h-16">4</div>
+        </div>
+      </div>
       {/* <Link to='/engagements' className='underline'>go to engagements</Link> */}
     </div>
   );
