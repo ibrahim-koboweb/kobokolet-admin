1cf8c329eca27ae2ab7543765562a9d5e6a636c1
Dan George <dangeorge35@gmail.com>
2023-02-24 21:00:03 +0100

    updated

--- src/pages/Others/Dashboard.jsx
+++ src/pages/Others/Dashboard.jsx
@@ -11,11 +11,17 @@ const Dashboard = () => {
         <div className="text-xl pt-1">Welcome to your dashboard</div>
       </p>
       <div className="mt-8">
-        <div className="grid grid-cols-4 grid-rows-1 gap-4">
+        <div className="grid lg:grid-cols-4 lg:grid-rows-1  xs:grid-cols-2 xs:grid-rows-2   gap-4">
           <div className="bg-white rounded p-3 h-16">1</div>
           <div className="bg-white rounded p-3 h-16">2</div>
           <div className="bg-white rounded p-3 h-16">3</div>
           <div className="bg-white rounded p-3 h-16">4</div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="grid lg:grid-cols-2 lg:grid-rows-1     gap-4">
+          <div className="bg-white rounded p-3 h-80">1</div>
+          <div className="bg-white rounded p-3 h-80">2</div>
         </div>
       </div>
       {/* <Link to='/engagements' className='underline'>go to engagements</Link> */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   theme: {
     screens: {
+      xs: "350px",
       sm: "480px",
       md: "768px",
       lg: "976px",
